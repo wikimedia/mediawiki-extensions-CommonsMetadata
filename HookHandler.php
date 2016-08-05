308b5ce6f140a0d8593813ce0f171bc88f6ac574
Reedy <reedy@wikimedia.org>
2016-08-05 18:16:32 +0100

    Remove 'UnitTestList' hook
    No longer needed now that extension unittests are autodiscovered.
    
    Bug: T142120
    Bug: T142121
    Change-Id: Ice06ea7f2c18d85420123ec8811e776e7d980715

--- HookHandler.php
+++ HookHandler.php
@@ -116,17 +116,6 @@ class HookHandler {
 	}
 
 	/**
-	 * Hook to add unit tests
-	 * @param array $files
-	 * @return bool
-	 */
-	public static function onUnitTestsList( &$files ) {
-		$testDir = __DIR__ . DIRECTORY_SEPARATOR . 'tests' . DIRECTORY_SEPARATOR . 'phpunit';
-		$files = array_merge( $files, glob( $testDir . DIRECTORY_SEPARATOR . '*Test.php' ) );
-		return true;
-	}
-
-	/**
 	 * @param Language $lang
 	 * @param bool $singleLang
 	 * @return DataCollector
