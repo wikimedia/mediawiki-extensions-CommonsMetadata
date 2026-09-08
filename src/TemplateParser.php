1ad0e7363d90477ea8a2cffa822f0f2169b70abf
Matthias Mullie <git@mullie.eu>
2026-09-08 09:00:53 +0200

    Fix invalid input into strtolower
    `parse_url` can return a lot of different
    types depending on the input. In this case,
    most non-url-like inputs would yield a
    `null` return value for scheme, which is
    invalid (deprecated) input into `strtolower`
    
    `false` is also possible, with e.g. `//` as
    input. While strtolower will not throw
    deprecation notices for that input, it is
    also not documented to be a type that is
    supported.
    
    This will simply convert any falsy return
    value to an empty string. Whatever is a valid
    scheme, should already always be a string as
    far as I'm aware.
    
    Bug: T435999
    Change-Id: I66c102cae36a9d499c95c37db7458371c7c87258
    (cherry picked from commit a175b27e48e401e8d3bea7888fbb612fde6c5b4f)

--- src/TemplateParser.php
+++ src/TemplateParser.php
@@ -399,13 +399,12 @@ class TemplateParser {
 				break;
 			}
 		}
-
 		// Filter out non-http(s) license urls
 		// https://phabricator.wikimedia.org/T435999
 		if (
 			isset( $data[ 'LicenseUrl' ] ) &&
 			!in_array(
-				strtolower( parse_url( $data[ 'LicenseUrl' ], PHP_URL_SCHEME ) ),
+				strtolower( parse_url( $data[ 'LicenseUrl' ], PHP_URL_SCHEME ) ?: '' ),
 				[ 'http', 'https' ]
 			)
 		) {
